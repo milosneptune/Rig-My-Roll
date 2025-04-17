c78871374461fff67477ac662d0184c3f49344aa
milo <miloqureshi02@gmail.com>
2025-04-17 15:53:20 -0400

    finished intializing symbol lists in slot

--- GamblerGame/Slot.cs
+++ GamblerGame/Slot.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 
@@ -12,46 +14,70 @@ namespace GamblerGame
 {
     enum SymbolName
     {
-        Cherry = 10,
+        Cherries = 10,
         Seven = 100,
         Lemon = 30,
         Lime = 50,
         Grape = 70,
         Pineapple = 60,
-        Watermelon = 10,
+        Melon = 10,
         Orange = 60,
         Kiwi = 50,
         Apple = 10,
         Strawberry = 10,
-        Banana = 30,
+        Bananas = 30,
     }
     internal class Slot
     {
         // list of symbol textures
-        private List<Texture2D> pictures;
-        private List<SymbolName> symbols;
+        //private List<Texture2D> pictures;
+        private List<Symbol> symbols; // the unchanged list of symbols
+        private List<Symbol> newSymbols; // this is the list that will change if an item is used
+
+        private ScriptManager sm;
 
         /// <summary>
         /// for getting the result of a roll from the slot 
         /// </summary>
-        public SymbolName Result { get; private set; }
+        public Symbol Result { get; private set; }
+        public SymbolName ResultName { get { return Result.Name; } }
+        
 
-        public Slot()
+        public Slot(ContentManager ct)
         {
-            // pictures will be intialized here from file using the script manager 
+            //symbols = new List<Symbol>();
+            //newSymbols = new List<Symbol>();
+            LoadSymbols(ct);
         }
 
         // method to use an item
 
+        // method to load symbolsnames into symbosl
+        private void LoadSymbols(ContentManager ct)
+        {
+
+            // check the SymbolsFile.txt, the scriptmanager needs
+            // to split them in the name and the texture, then the Symbol() class
+            // needs to intialize each of them with it's name and appropriate texture, 
+            // then it needs to be added to both the symbols list and new symbols
+
+            sm = new ScriptManager("SymbolsFile");
+            List<string> names = new List<string>();
+            names = sm.GetNames();
+            foreach (string name in names)
+            {
+                symbols.Add(new Symbol(Enum.Parse<SymbolName>(name), ct));
+                newSymbols.Add(new Symbol(Enum.Parse<SymbolName>(name), ct));
+            }
+
+        }
 
         // method to roll the slot
         public void Roll(Random rng)
         {
-
             // if their as item being used, the use item method will manipulate the private list
             // example, an item increases the chance of cherry being rolled, then the use item method 
             // would replace one of the symbol names with cherry
-
             Result = symbols[rng.Next(1, symbols.Count)];
         }
 
