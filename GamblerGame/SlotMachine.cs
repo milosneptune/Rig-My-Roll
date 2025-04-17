edaeeeed6d9d40649a37dd1d9a7dab4c8e3db8b5
mjclatts <mc5719@rit.edu>
2025-04-17 19:24:19 -0400

    Updated Check Multiply

--- GamblerGame/SlotMachine.cs
+++ GamblerGame/SlotMachine.cs
@@ -58,11 +58,11 @@ namespace GamblerGame
             {
                 matchedSymbols++;
             }
-            else if (slots[0].Result == slots[2].Result)
+            if (slots[0].Result == slots[2].Result)
             {
                 matchedSymbols++;
             }
-            else if (slots[1].Result == slots[2].Result)
+            if (slots[1].Result == slots[2].Result)
             {
                 matchedSymbols++;
             }
