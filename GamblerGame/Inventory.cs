e6aba52216d5a35156e2f5ea1c3538c6d01b3075
Yen-Zhu <yeyanzhu10@gmail.com>
2025-04-27 22:12:24 -0400

    Add Inventory class, fix money problem

--- GamblerGame/Inventory.cs
+++ GamblerGame/Inventory.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,14 +12,43 @@ namespace GamblerGame
 {
     internal class Inventory
     {
+        private Vector2 boxOne;
+        private Vector2 boxTwo;
+        private Vector2 boxThree;
+        private Vector2 boxFour;
+        private Vector2 boxFive;
+        private List<Item> items;
+
         public List<Item> Items { get; private set; }
 
-        
-        public void UseItem()
+        public Inventory()
         {
 
         }
 
+        /// <summary>
+        /// Adds item to the item list.
+        /// </summary>
+        /// <param name="item"></param>
+        public void Add(Item item)
+        {
+            items.Add(item);
+        }
+        /// <summary>
+        /// Removes item from list.
+        /// </summary>
+        /// <param name="item"></param>
+        public void Remove(Item item)
+        {
+            items.Remove(item);
+        }
+        public void Update(GameTime gameTime)
+        {
 
+        }
+        public void Draw(SpriteBatch spriteBatch)
+        {
+
+        }
     }
 }
